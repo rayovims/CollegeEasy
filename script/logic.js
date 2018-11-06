c331cdc7705b4c5bc9d51a54539d1a42c6e380f0
Raymundo <rayovims@gmail.com>
2018-11-06 13:12:35 -0500

    it can read files

--- script/logic.js
+++ script/logic.js
@@ -1,3 +1,17 @@
-if (FileReader) {
-    console.log(FileReader);
-}
+// $(".submit").on("click", function(e) {
+//   e.preventDefault();
+//   var fr = new FileReader();
+//   fr.onload = function () {
+//     $("#fileContents").html(this.result)
+//   }
+//   fr.readAsText
+// });
+
+document.getElementById("openFile").addEventListener("change", function () {
+  var fr = new FileReader();
+  fr.onload = function () {
+    // document.getElementById("fileContents").textContent = this.result;
+    console.log(this.result);
+  }
+  fr.readAsText(this.files[0]);
+});
